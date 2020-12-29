0bc0760562f295833b550f07e6f6f4e3e3f4fc62
Matvey Nurminen <mat.nurminen@hotmail.com>
2020-12-30 00:09:37 +0200

    29.11_2

--- client/src/components/layout/mainCards.js
+++ client/src/components/layout/mainCards.js
@@ -3,47 +3,48 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const images = [
   {
     url: '/img/nhl19.jpg',
     title: 'Season 2019-20',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl18.jpg',
     title: 'Season 2018-19',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl17.jpg',
     title: 'Season 2017-18',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl16.jpg',
     title: 'Season 2016-17',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl15.jpg',
     title: 'Season 2015-16',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl14.jpg',
     title: 'Season 2014-15',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl13.jpg',
     title: 'Season 2013-12',
-    width: '50%',
+    link: '/leagues',
   },
   {
     url: '/img/nhl12.jpg',
     title: 'Season 2012-13',
-    width: '50%',
+    link: '/leagues',
   },
 ];
 
@@ -55,8 +56,19 @@ const useStyles = makeStyles((theme) => ({
   boxMain: {
     margin: 10,
     position: 'relative',
+    display: 'inline - block',
+    overflow: 'hidden',
   },
   imgMain: {
+    borderRadius: 10,
+    width: '100%',
+    transition: '0.3s',
+    display: 'block',
+    '&:hover': {
+      transform: 'scale(1.1)',
+    },
+  },
+  img: {
     borderRadius: 10,
     width: '100%',
   },
@@ -73,40 +85,44 @@ export default function ButtonBases() {
       <Grid container>
         <Grid item xs={12}>
           <Box className={classes.boxMain}>
-            <img className={classes.imgMain} src='/img/nhl20.jpg' alt='' />
-            <div
-              style={{
-                position: 'absolute',
-                color: 'white',
-                bottom: 2,
-                left: '50%',
-                transform: 'translateX(-50%)',
-              }}
-            >
-              <Typography variant='h3' className={classes.fontMain}>
-                Season 2020-21
-              </Typography>
-            </div>
+            <Box component={Link} to='/leagues'>
+              <img className={classes.imgMain} src='/img/nhl20.jpg' alt='' />
+              <div
+                style={{
+                  position: 'absolute',
+                  color: 'white',
+                  bottom: 5,
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                }}
+              >
+                <Typography variant='h3' className={classes.fontMain}>
+                  Season 2020-21
+                </Typography>
+              </div>
+            </Box>
           </Box>
         </Grid>
 
         {images.map((image) => (
           <Grid item xs={12} sm={6}>
             <Box className={classes.boxMain}>
-              <img className={classes.imgMain} src={image.url} alt='' />
-              <div
-                style={{
-                  position: 'absolute',
-                  color: 'white',
-                  bottom: 2,
-                  left: '50%',
-                  transform: 'translateX(-50%)',
-                }}
-              >
-                <Typography variant='h5' className={classes.fontMain}>
-                  {image.title}
-                </Typography>
-              </div>
+              <Box component={Link} to={image.link}>
+                <img className={classes.img} src={image.url} alt='' />
+                <div
+                  style={{
+                    position: 'absolute',
+                    color: 'white',
+                    bottom: 2,
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                  }}
+                >
+                  <Typography variant='h5' className={classes.fontMain}>
+                    {image.title}
+                  </Typography>
+                </div>
+              </Box>
             </Box>
           </Grid>
         ))}
