00f3a69768b9d050aeba62c4dba821eb68be9bed
Matvey Nurminen <mat.nurminen@hotmail.com>
2020-12-30 21:24:48 +0200

    chart

--- client/src/components/countries/chart.js
+++ client/src/components/countries/chart.js
@@ -39,17 +39,6 @@ const styles = (theme) => ({
   },
 });
 
-// const data = {
-//   labels: ['Red', 'Blue', 'Yellow'],
-//   datasets: [
-//     {
-//       data: [300, 50, 100],
-//       backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-//       hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-//     },
-//   ],
-// };
-
 export class CountryChart extends Component {
   constructor(props) {
     super(props);
